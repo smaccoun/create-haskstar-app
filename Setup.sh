d828388ad34d290035a4564fd4e1a0f481dd0ced
Steven MacCoun <theraccoun@gmail.com>
2018-03-22 11:33:23 -0600

    Back-end setup

--- Setup.sh
+++ Setup.sh
@@ -1,7 +1,27 @@
-git clone git@github.com:simonh1000/elm-webpack-starter.git
+echo "Setting up Project"
 
-mv elm-webpack-starter web
+echo "***********************"
+echo " Setting up Front End   "
+echo "***********************"
 
-rm web/README.md
+if [ -d ./front-end ]; then
+    echo "Looks like you already have a web directory"
+else
+	git clone git@github.com:simonh1000/elm-webpack-starter.git
 
+	mv elm-webpack-starter front-end
 
+	rm front-end/README.md
+fi
+
+echo "***********************"
+echo " Setting up Back End   "
+echo "***********************"
+
+if [ -d ./back-end ]; then
+    echo "Looks like you already have a back-end"
+else
+	git clone git@github.com:smaccoun/haskstar-haskell.git
+
+	mv haskstar-haskell back-end
+fi
