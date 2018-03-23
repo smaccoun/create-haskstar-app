05ae56ebaa80c5a5bc642040d8a91be282399394
Steven MacCoun <theraccoun@gmail.com>
2018-03-23 14:18:42 -0600

    Install front-end packages

--- create-app.hs
+++ create-app.hs
@@ -10,13 +10,13 @@ data DirSetup =
     ,gitDir :: Text
     }
 
-frontEndDirSetup =
+frontendDirConfig =
   DirSetup
 	  {dirName = "front-end"
 	  ,gitDir = "git@github.com:simonh1000/elm-webpack-starter.git"
 	  }
 
-backEndDirSetup =
+backendDirConfig =
   DirSetup
 	  {dirName = "back-end"
 	  ,gitDir = "git@github.com:smaccoun/haskstar-haskell.git"
@@ -28,24 +28,37 @@ majorCommentBlock :: Text -> IO ()
 majorCommentBlock msg = do
   putStrLn "******************"
   echo $ unsafeTextToLine msg 
-  putStrLn "******************"
+  putStrLn "******************\n\n"
 
+getDir :: Turtle.FilePath -> DirSetup -> (Text, Turtle.FilePath)
+getDir rootDir dirSetup =
+  (dname, dPath)
+  where
+    dname = dirName dirSetup
+    dPath = rootDir </> (fromText dname)
+
+setupDir :: Turtle.FilePath -> DirSetup -> IO ()
 setupDir rootDir dirSetup = do
-  let dname = dirName dirSetup
-  let dPath = rootDir </> (fromText dname)
+  let (dname, dPath) = getDir rootDir dirSetup
   majorCommentBlock $ "Setting up " <> dname
-  mkdir dPath
-  cd dPath
-  setupResult<- shell ("git clone " <> gitDir dirSetup) empty
+  setupResult<- shell ("git clone " <> gitDir dirSetup <> " " <> dname) empty
   case setupResult of
-	ExitSuccess   -> return ()
-	ExitFailure n -> die (" failed with exit code: " <> repr n)
-  
+    ExitSuccess   -> return ()
+    ExitFailure n -> die (" failed with exit code: " <> repr n)
+
+buildFrontEnd topDir = do
+  majorCommentBlock "BUILDING FRONT END"
+  cd $ getDir topDir frontendDirConfig & snd
+  _ <- shell "yarn install" empty
+  _ <- shell "elm-package install --yes" empty
+  return ()
 
 main :: IO ()
 main = do
   topDir <- pwd
-  setupDir topDir backEndDirSetup
-  setupDir topDir frontEndDirSetup 
+  setupDir topDir backendDirConfig
+  setupDir topDir frontendDirConfig
+  buildFrontEnd topDir
+  cd topDir
   return ()
 
