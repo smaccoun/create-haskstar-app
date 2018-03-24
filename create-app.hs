717bedb63074655e1655ba6d46ae7d11fd756ad9
Steven MacCoun <theraccoun@gmail.com>
2018-03-24 14:29:52 -0600

    Prompt for existing dirs and open servers in new tabs

--- create-app.hs
+++ create-app.hs
@@ -7,11 +7,10 @@ import Turtle
 
 main :: IO ()
 main = do
+  shell "chmod +x ttab" empty
   topDir <- pwd
-  setupDir topDir backendDirConfig
-  setupDir topDir frontendDirConfig
-  buildBackEnd topDir
-  buildFrontEnd topDir
+  validateAndSetupDir topDir backendDirConfig
+  validateAndSetupDir topDir frontendDirConfig
   runFrontEnd topDir
   cd topDir
   return ()
@@ -19,21 +18,26 @@ main = do
 
 data DirSetup =
   DirSetup
-    {dirName :: Text
+    {dirStackType :: DirStackType
+    ,dirName :: Text
     ,gitDir :: Text
     }
+
+data DirStackType = FRONT_END | BACK_END
 
 frontendDirConfig :: DirSetup
 frontendDirConfig =
   DirSetup
-	  {dirName = "front-end"
+	  {dirStackType = FRONT_END
+	  ,dirName = "front-end"
 	  ,gitDir = "git@github.com:smaccoun/haskstar-elm.git"
 	  }
 
 backendDirConfig :: DirSetup
 backendDirConfig =
   DirSetup
-	  {dirName = "back-end"
+	  {dirStackType = BACK_END
+	  ,dirName = "back-end"
 	  ,gitDir = "git@github.com:smaccoun/haskstar-haskell.git"
 	  }
 
@@ -53,16 +57,58 @@ getDir rootDir dirSetup =
     dname = dirName dirSetup
     dPath = rootDir </> (fromText dname)
 
+data ValidateResult = Valid | Replace | Keep
+
+validateInitialSetup :: Text -> Turtle.FilePath -> IO ValidateResult
+validateInitialSetup dname dPath = do
+  existingDir <- testdir dPath
+  if existingDir then do
+    echo $ unsafeTextToLine $ "Found existing directory " <> dname
+    echo "Would you like to replace (R) or keep (enter)"
+    answer <- readline
+    case answer of
+        Just a ->
+            case a of
+                "R" -> do
+                    echo "Replacing existing directories"
+                    return Replace
+                _ -> do
+                  echo "Keeping existing directory"
+                  return Keep
+  else do
+    echo "Detected valid initial state"
+    return Valid
+
+validateAndSetupDir :: Turtle.FilePath -> DirSetup -> IO ()
+validateAndSetupDir rootDir dirSetup = do
+    let (dname, dPath) = getDir rootDir dirSetup
+    majorCommentBlock $ "Setting up " <> dname
+    validateResult <- validateInitialSetup dname dPath
+    case validateResult of
+        Valid -> do
+            setup
+            return ()
+        Replace -> do
+            rmtree dPath
+            setup
+        Keep -> return ()
+    where
+        setup = do
+            setupDir rootDir dirSetup
+            return ()
+
+
 setupDir :: Turtle.FilePath -> DirSetup -> IO ()
 setupDir rootDir dirSetup = do
   let (dname, dPath) = getDir rootDir dirSetup
-  existingDir <- testdir dPath
-  if existingDir then do
-    echo "Found existing directory! Will remove and replace!"
-    rmtree dPath
-  else
-    echo "Detected valid initial state"
-  majorCommentBlock $ "Setting up " <> dname
+  getTemplate dname dirSetup
+  case dirStackType dirSetup of
+    FRONT_END -> buildFrontEnd rootDir
+    BACK_END -> buildBackEnd rootDir
+
+
+
+getTemplate dname dirSetup = do
   setupResult <- shell ("git clone " <> gitDir dirSetup <> " " <> dname) empty
   case setupResult of
     ExitSuccess   -> return ()
@@ -84,8 +130,13 @@ buildBackEnd topDir = do
 
 runFrontEnd topDir = do
   majorCommentBlock "STARTING WEB SERVER"
+  cd $ getDir topDir backendDirConfig & snd
+  s <- shell "../ttab stack exec api-exe" empty
+
   cd $ getDir topDir frontendDirConfig & snd
-  s <- shell "yarn start & " empty
+  s <- shell "../ttab yarn start " empty
+
+
   case s of
     ExitSuccess   -> do
         printf "\nSuccessfully started server. Go to localhost:3000\n"
