0396bbf1a88bd5c74475471094dda6e747ebe052
Steven MacCoun <theraccoun@gmail.com>
2018-03-28 10:08:47 -0600

    Split out interactive shell functions

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,32 +8,10 @@ import Turtle
 import Filesystem.Path.CurrentOS (encodeString)
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
-import Data.Maybe
 
-prompt :: T.Text -> Maybe T.Text -> IO T.Text
-prompt promptQuestion mbDefault = do
-  let questionExtension = fmap (\d -> " (default " <> d <> ")") mbDefault & fromMaybe ""
-  let question = promptQuestion <> questionExtension <> ": "
-  putStrLn "\n-----------------------"
-  TIO.putStrLn question
-  getAnswer
-  where
-    getAnswer = do
-      mbAnswer <- readline
-      case mbAnswer of
-        Just answer -> do
-          let asText = lineToText answer
-          let didHitEnter = (asText == T.empty)
-          if didHitEnter then
-               case mbDefault of
-                  Just def -> do
-                    TIO.putStrLn $ "---> Nothing entered. Using default " <> def
-                    return def
-                  Nothing -> getAnswer
-          else
-            return (lineToText answer)
-        Nothing ->
-          getAnswer
+
+import Interactive
+
 
 
 askToRun :: IO () -> IO ()
@@ -77,11 +55,7 @@ backendDirConfig =
 
 asLocal dirName = "./" <> dirName
 
-majorCommentBlock :: Text -> IO ()
-majorCommentBlock msg = do
-  printf "\n\n***********************************************\n"
-  echo $ unsafeTextToLine msg
-  printf "***********************************************\n\n"
+
 
 
 getDir :: Turtle.FilePath -> DirSetup -> (Text, Turtle.FilePath)
