2a4a08c2b0ba030c2e060e9d4acb766a599948fb
Steven MacCoun <theraccoun@gmail.com>
2018-04-19 14:57:20 -0600

    Change circle deploy to default aws

--- core-script-src/src/Deploy.hs
+++ core-script-src/src/Deploy.hs
@@ -21,22 +21,27 @@ data DeployConfig =
     }
 
 deploy :: DeployConfig -> ScriptRunContext ()
-deploy (DeployConfig (RemoteDockerBaseDir remoteDockerBaseDir) (SHA1 sha1)) = do
+deploy (DeployConfig (RemoteDockerBaseDir remoteDockerBaseDir) sha1) = do
   fromAppRootDir
   cd "back-end"
   _ <- shell dockerBuildRelative empty
   _ <- shell dockerPushRelative empty
   return ()
   where
-    dockerBuildRelative = dockerBuildStrCmd remoteDockerDir "Dockerfile ."
-    dockerPushRelative = dockerPushCmd remoteDockerDir
-    remoteDockerDir = remoteDockerBaseDir <> sha1
+    dockerBuildRelative = dockerBuildStrCmd remoteDockerBaseDir "Dockerfile ."
+    dockerPushRelative = dockerPushCmd remoteDockerBaseDir (Just sha1)
 
 
 dockerBuildStrCmd :: Text -> Turtle.FilePath -> Text
 dockerBuildStrCmd remoteDockerDir localDockerFile =
   format ("docker build --rm=false -t "%s%" -f "%s%"") remoteDockerDir (pack $ encodeString localDockerFile)
 
-dockerPushCmd :: Text -> Text
-dockerPushCmd remoteDockerDir =
-  format ("docker push "%s%"") remoteDockerDir
+dockerPushCmd :: Text -> Maybe SHA1 -> Text
+dockerPushCmd remoteDockerDir maybeSha1 =
+  format ("docker push "%s%"") remoteDir
+  where
+    remoteDir = remoteDockerDir <> ":" <> tag
+    tag =
+        case maybeSha1 of
+          Just (SHA1 sha1) -> sha1
+          Nothing          -> ""
