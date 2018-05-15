79c28cb07d35ea6f54ab34fab3bea36dc917fb01
Steven MacCoun <theraccoun@gmail.com>
2018-05-15 16:31:21 -0600

    Audit column on user table

--- templates/db/migrations/haskell/pg-simple/migrations/001.sql
+++ templates/db/migrations/haskell/pg-simple/migrations/001.sql
@@ -10,5 +10,10 @@ CREATE TABLE users (
   created_at TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL
 );
 
-INSERT INTO users (email, password)
-VALUES ('admin@haskstar.com', '14|8|1|5CmOii8RilPuv6vDZFFcPDX8WyfYKnhV+eYcRDwadvM=|bcvjyIY/O3PUZpeorOLqGPOLM2ind2fuEvY1B4ATrAJQGhNqNFWjxRs0W51zxrwMxE4imhhA9Fn1lTSgqkWvkQ==');
+INSERT INTO users (email, password, updated_at, created_at)
+VALUES 
+  ('admin@haskstar.com'
+  , '14|8|1|5CmOii8RilPuv6vDZFFcPDX8WyfYKnhV+eYcRDwadvM=|bcvjyIY/O3PUZpeorOLqGPOLM2ind2fuEvY1B4ATrAJQGhNqNFWjxRs0W51zxrwMxE4imhhA9Fn1lTSgqkWvkQ=='
+  , now()
+  , DEFAULT
+  );
