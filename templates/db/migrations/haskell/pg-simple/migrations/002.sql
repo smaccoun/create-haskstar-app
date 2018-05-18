168cdd8f03b90de3e632429a21cf71d6efa52587
Steven MacCoun <theraccoun@gmail.com>
2018-05-17 19:30:38 -0600

    Audit column on blog post

--- templates/db/migrations/haskell/pg-simple/migrations/002.sql
+++ templates/db/migrations/haskell/pg-simple/migrations/002.sql
@@ -1,6 +1,6 @@
 CREATE TABLE public.blog_posts
 (
-  blog_post_id UUID DEFAULT gen_random_uuid() PRIMARY KEY NOT NULL,
+  id UUID DEFAULT gen_random_uuid() PRIMARY KEY NOT NULL,
   title TEXT NOT NULL,
   content TEXT,
   updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
